6026f13eba5353be94c6337ceed21f52e7533d12
chuongnguyen594 <nguyendinhchuong2909@gmail.com>
2026-08-14 20:21:26 +0700

    Fix correct API URL

--- javascript.js
+++ javascript.js
@@ -6,8 +6,8 @@ async function api(action, params = {}) {
   });
 
   const url =
-    "https://script.google.com/macros/s/AKfycbzlhETATxd9MHSD1Ce3e_uIqtD-sTSAA0nu6W1Iia6dXjYi6Ecv35jTYujelPkoJRIAaA/exec?" +
-    query.toString();
+  "https://script.google.com/macros/s/AKfycbzlhETATxd9MHSD1Ce3e_uIqtD-sTSAAOnu6W1Iia6dXjYi6Ecv35jTYujelPkoJRIAaA/exec?" +
+  query.toString();
 
   const response = await fetch(url, {
     method: "GET",
